8dad88c34e93fb1174beda5dd483feb3c2776312
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-01-01 01:49:34 -0500

    update yum-dell description

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 maintainer       'Eric G. Wolfe'
 maintainer_email 'wolfe21@marshall.edu'
 license          'Apache 2.0'
-description      'Installs and configures EPEL, ELFF, Dell, and VMware yum repositories.'
+description      'Configures Dell repositories. Installs OMSA'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.0.1'
 depends          'yum'
 name             'yum-dell'
 recipe 'yum-dell::default', 'Installs Dell OpenManage and optionally firmware components.'
