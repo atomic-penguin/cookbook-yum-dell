b018cd4dfd92d172b0dc116411155e79a0f72be8
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-01-02 17:48:17 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'wolfe21@marshall.edu'
 license          'Apache 2.0'
 description      'Configures Dell repositories. Installs OMSA'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.1'
+version          '3.0.2'
 depends          'yum'
 name             'yum-dell'
 recipe 'yum-dell::default', 'Installs Dell OpenManage and optionally firmware components.'
