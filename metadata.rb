02ddeacad7a18dab9faac1d1a300a04aff726b4f
Peter Eisentraut <peter@eisentraut.org>
2014-08-13 14:19:58 -0400

    Improve yum cookbook dependency
    This cookbook requires yum >=3, because it uses the gpgkey attribute, so
    put that into the metadata.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          'Apache 2.0'
 description      'Configures Dell repositories. Installs OMSA'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.4'
-depends          'yum'
+depends          'yum', '>= 3'
 name             'yum-dell'
 recipe 'yum-dell::default', 'Installs Dell OpenManage and optionally firmware components.'
 recipe 'yum-dell::omsa', 'Reverses default recipe'
